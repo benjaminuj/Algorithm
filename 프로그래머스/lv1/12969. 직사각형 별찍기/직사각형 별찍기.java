0c366038bfca82914858ec32646aba14eefeb72f
benjamin <chosj1526@gmail.com>
2023-03-15 19:16:43 +0900

    [level 1] Title: 직사각형 별찍기, Time: 219.80 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for(int i=0; i<m; i++) {
+            for(int k=0; k<n; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
