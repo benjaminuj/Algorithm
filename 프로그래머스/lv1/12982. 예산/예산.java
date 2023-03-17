b56b340c2de05edf4f47d246095705d3bc8f6e15
benjamin <chosj1526@gmail.com>
2023-03-17 21:07:39 +0900

    [level 1] Title: 예산, Time: 0.38 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for(int i=0; i<d.length; i++) {
+            budget-=d[i];
+            if(budget>=0) answer++;
+        }
+        return answer;
+    }
+}
