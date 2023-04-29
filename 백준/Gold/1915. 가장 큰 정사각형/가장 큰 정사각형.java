70ea3cdfd52442c876c474f5bd3bc3c08bb78a14
benjamin <chosj1526@gmail.com>
2023-04-29 17:14:19 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 588 ms, Memory: 62236 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -1,9 +1,5 @@
 import java.io.IOException;
 import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
 	public static void main(String[] args) throws IOException {
 		long[][] D = new long[1001][1001];
@@ -15,14 +11,35 @@ public class Main {
 			String mline = sc.next();
 			for(int j=0; j<m; j++) {
 				D[i][j] = Long.parseLong(String.valueOf(mline.charAt(j)));
-				if(D[i][j] == 1 && j>0 && i>0) {
-					D[i][j] = Math.min(D[i-1][j-1] , Math.min(D[i-1][j],D[i][j-1])) + 1;
-				}
-				if(max < D[i][j]) {
-					max = D[i][j];
+			}
+		}
+		max = dp(n,m, D);
+		System.out.println(max*max);
+	}
+	
+	public static long dp(int n, int m, long[][] D) {
+		long max = 1;
+		boolean isOneExistence = false;
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				if(D[i][j] != 0) {
+					isOneExistence = true;
+					if(i >0 && j>0) {
+						if(D[i-1][j] != 0 && D[i][j-1] != 0 && D[i-1][j-1] != 0) {
+//							if(D[i-1][j] == D[i][j-1] && D[i-1][j] == D[i-1][j-1] && D[i][j-1] == D[i-1][j-1]) {
+//								D[i][j] = ++max;
+//								continue;
+//							}
+							D[i][j] = Math.min(D[i-1][j-1] , Math.min(D[i-1][j],D[i][j-1])) + 1;
+							if(max < D[i][j]) {
+								max = D[i][j];
+							}
+						}
+					}
 				}
 			}
 		}
-			System.out.println(max*max);
+		if(!isOneExistence)max = 0;
+		return max;
 	}
 }
