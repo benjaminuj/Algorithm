441f3c06478e7504969fcecb23b5ba1c1afb8bdf
YuJin Cho <chosj1526@gmail.com>
2023-09-20 23:54:29 +0900

    [level 1] Title: 폰켓몬, Time: 3.36 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,31 @@
-import java.util.*;
-
 class Solution {
+    static boolean[] peak;
     public int solution(int[] nums) {
         int answer = 0;
-		int n = nums.length/2;
-
-		HashSet<Integer> hashSet = new HashSet<Integer>();
-		for(int i=0; i<nums.length; i++) {
-			hashSet.add(nums[i]);
-		}
-		if(n < hashSet.size()) answer = n;
-		else answer = hashSet.size();
-		return answer;
+        int N = nums.length;
+        Map<Long, Long> pocketmon = new HashMap<>();
+        peak = new boolean[pocketmon.size()+1];
+        for(int i=0; i<N; i++) {
+            pocketmon.put(nums[i], i);
+        }
+        
+        combination(pocketmon, N, 0, N/2);
+        return answer;
+    }
+    
+    public void combination(Map<Long, Long> pocketmon, int N, int index, int r) {
+        if(r == 0) {
+            for(int i=1; i<=N; i++) {
+                if(peak[i]) count++;
+            }
+        }
+        
+        if(index == N) return;
+        
+        peak[index] = true;
+        combination(pocketmon, N, index+1, r-1);
+        
+        peak[index] = false;
+        combination(pocketmon, N, index+1, r);
     }
 }
