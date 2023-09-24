46443f102773c44a20dbea4e87cee28dc16b2479
YuJin Cho <chosj1526@gmail.com>
2023-09-24 16:00:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 108.43 ms, Memory: 95.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.Map.Entry;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> count = new HashMap<>();
+        
+        for(int i =0; i<participant.length; i++) {
+            count.put(participant[i], count.getOrDefault(participant[i], 0) +1);
+        }
+        
+        for(int i=0; i<completion.length; i++) {
+            count.put(completion[i], count.get(completion[i])-1);
+        }
+         
+        count.values().removeIf(sum -> sum==0);
+        
+        Iterator<String> iter = count.keySet().iterator();
+        answer = iter.next();
+    
+        return answer;
+    }
+}
