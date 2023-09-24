a7c166e21a5e28852d583d5472dc3ec99094d9b1
YuJin Cho <chosj1526@gmail.com>
2023-09-24 18:29:50 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.java
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -6,14 +6,25 @@ class Solution {
         Map<String, Integer> count = new HashMap<>();
         
         for(int i=0; i<clothes.length; i++) {
-            count.put(clothes[i][1], count.getOrDefault(clothes[i][1], 1) +1);
+            count.put(clothes[i][1], count.getOrDefault(clothes[i][1], 0) +1);
         }
-        long temp = 1;
         
-        for(Integer cnt : count.values()) {    
-            temp *= cnt; 
+        //DP
+        Integer[] cnt = new Integer[count.size()];
+
+        count.values().toArray(cnt);
+
+        long[][] dp = new long[count.size()][2];
+        dp[0][0] = 1;
+        dp[0][1] = cnt[0];
+        
+        for(int i=1;i <dp.length; i++) {
+            dp[i][0] = dp[i-1][0] + dp[i-1][1];
+            dp[i][1] = dp[i-1][0]*cnt[i] + dp[i-1][1]*cnt[i];
         }
-        answer = (int) (temp-1);
+        
+        answer = (int) (dp[dp.length-1][0] + dp[dp.length-1][1] -1);
+    
         return answer;
     }
 }
