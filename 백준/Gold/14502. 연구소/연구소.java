6f5cdd2ac1912d1ce3771132032486da6effe30d
YuJin Cho <chosj1526@gmail.com>
2024-01-17 15:17:19 +0900

    [Gold IV] Title: 연구소, Time: 488 ms, Memory: 75188 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -2,114 +2,112 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static int[] dy = {-1,0,1,0};
-	public static int[] dx = {0,1,0,-1};
-	public static int N, M;
-	public static int[][] visited;
-	public static int[][] map;
-	public static int[][] virusMap;
-    public static int answer = Integer.MIN_VALUE;
-    public static List<Info> wall = new ArrayList<>();
-    public static Queue<Info> viruses = new LinkedList<>();
-	
-	public static class Info {
-		int x;
-		int y;
-		
-		public Info(int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-	}
+	static String[][] grid;
+	static boolean[][] visited;
+	static int[][]newWall = new int[3][2];
+	static int[][] dr = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+	static int virusCnt;
+	static int minVirus = Integer.MAX_VALUE;
+	static List<int[]> virus = new ArrayList<>();
 	
 	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		grid = new String[N][M];
+		visited = new boolean[N][M];
+		int baseOneCnt = 0;
 		
-		
-		for(int i=0; i<N; i++) {
-			st =  new StringTokenizer(br.readLine());
-			for(int j=0; j<M ; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 0) wall.add(new Info(i,j));
-			}
-		}
-		
-		insertWall(map);
-		
-		System.out.print(answer);
-		
-	}
-	
-	public static void insertWall(int[][] map) {
-		for(int i=0; i<wall.size()-2; i++) {
-			for(int j=i+1; j<wall.size()-1; j++) {
-				for(int k=j+1; k<wall.size(); k++) {
-					map[wall.get(i).y][wall.get(i).x] = 1;
-					map[wall.get(j).y][wall.get(j).x] = 1;
-					map[wall.get(k).y][wall.get(k).x] = 1;
-					answer = Math.max(answer, bfs());
-					
-					map[wall.get(i).y][wall.get(i).x] = 0;
-					map[wall.get(j).y][wall.get(j).x] = 0;
-					map[wall.get(k).y][wall.get(k).x] = 0;
-					
+		for (int r = 0; r < N; r++) {
+			st = new StringTokenizer(br.readLine());
+			for (int c = 0; c < M; c++) {
+				grid[r][c] = st.nextToken();
+				if (grid[r][c].equals("1")) {
+					baseOneCnt++;
+				}
+				if (grid[r][c].equals("2")) {
+					virus.add(new int[] {r,c});
 				}
 			}
 		}
-	}
-	
-	public static int bfs() {
-		int result = 0;
-		virusMap = new int[N][M];
-		visited = new int[N][M];
-		viruses.clear();
 		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				virusMap[i][j] = map[i][j];
-				if(virusMap[i][j] == 2) viruses.add(new Info(i,j));
+		// 벽 3개 둘 수 있는 조합구하기
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < M; c++) {
+				if (grid[r][c].equals("0")) {
+					backtracking(r, c, 1);
+				}
 			}
 		}
-		
-		
-		while(!viruses.isEmpty()) {
+		System.out.println(N*M - (baseOneCnt + 3) - minVirus);
+	}
+	
+	// 감염 로직 bfs
+	public static void bfs() {
+		Queue<int[]> q = new ArrayDeque<>();
+		for (int[] vir : virus) {
+			q.offer(vir);
+			visited[vir[0]][vir[1]] = true;
+			virusCnt++;
+		}
+	
+		while (!q.isEmpty()) {
+			int[] now = q.poll();
 			
-			Info virus = viruses.poll();
-			int x = virus.x;
-			int y = virus.y;
-			visited[y][x] = 1;
-			
-			
-			for(int i=0; i<4; i++) {
-				int nextX = x + dx[i];
-				int nextY = y + dy[i];
+			for (int[] d : dr) {
+				int nextR = now[0] + d[0];
+				int nextC = now[1] + d[1];
 				
-				if(nextX < 0 || nextY < 0 || nextX >= M || nextY >= N) continue;
-				if(virusMap[nextY][nextX] == 0 && visited[nextY][nextX] != 1) {
-					
-					visited[nextY][nextX] = 1;
-					virusMap[nextY][nextX] = 2;
-					viruses.add(new Info(nextY,nextX));
-					
+				if (isValid(nextR, nextC) && grid[nextR][nextC].equals("0") && !visited[nextR][nextC] && !isNewWall(nextR, nextC)) {
+					q.offer(new int[]{nextR, nextC});
+					virusCnt++;
+					visited[nextR][nextC] = true;
 				}
 			}
 		}
+	}
+	
+	// 벽 3개 조합 구하기
+	public static void backtracking(int r, int c, int cnt) {
+		newWall[cnt-1][0] = r;
+		newWall[cnt-1][1] = c;
 		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				if(virusMap[i][j] == 0) result++;
+		if (cnt == 3) {
+			bfs();
+			minVirus = Math.min(minVirus, virusCnt);
+			for (boolean[] a : visited) {
+				Arrays.fill(a, false);
+			}
+			virusCnt = 0;
+			return;
+		}
+		
+		for (int i = r; i < grid.length; i++) {
+			for (int j = c; j < grid[0].length; j++) {
+				int nextC = (j+1) % grid[0].length; // 
+				int nextR = i;
+				if (j + 1 >= grid[0].length) {
+					nextR++;
+				}		
+				
+				if (isValid(nextR, nextC) && grid[nextR][nextC].equals("0")) {
+					backtracking(nextR, nextC, cnt+1);
+				}
+				c = 0;	
 			}
 		}
-		return result;
-		
 	}
-		
+	
+	public static boolean isValid(int r, int c) {
+		return r < grid.length && r >= 0 && c < grid[0].length && c >= 0;
+	}
+	
+	public static boolean isNewWall(int r, int c) {
+		return (r == newWall[0][0] && c == newWall[0][1]) || (r == newWall[1][0] && c == newWall[1][1]) || (r == newWall[2][0] && c == newWall[2][1]);
+	}
 
 }
-
-
