40958d5b6711c7fc88c5f65253f2ec3acc56ae33
YuJin Cho <chosj1526@gmail.com>
2024-05-26 05:45:40 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.39 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -10,61 +10,27 @@ class Solution {
             char c = str.charAt(0);
             switch(c) {
                 case 'I' : 
-                    if (minQ.size() != 0) minQ.offer(Integer.parseInt(str.substring(2, str.length()))); 
-                    else if (maxQ.size() != 0) maxQ.offer(Integer.parseInt(str.substring(2, str.length()))); 
-                    else minQ.offer(Integer.parseInt(str.substring(2, str.length()))); 
-                    
+                    minQ.offer(Integer.parseInt(str.substring(2, str.length()))); 
+                    maxQ.offer(Integer.parseInt(str.substring(2, str.length()))); 
                     break;
+                    
                 case 'D' :
                     if (minQ.size() == 0 && maxQ.size() == 0) break;
+                    
                     char i = str.charAt(2);
-                    
                     if (i == '1') {
-                        if (minQ.size() != 0) {
-                            maxQ.clear();
-                            
-                            Iterator iter = minQ.iterator();
-                            while (iter.hasNext()) {
-                                maxQ.offer((Integer)(iter.next()));
-                            }
-                        }
-                        minQ.clear();
-                        maxQ.remove();
+                        minQ.remove(maxQ.remove());
                     }
-                    
                     if (i == '-') {
-                        if (maxQ.size() != 0) {
-                            minQ.clear();
-                            
-                            Iterator iter = maxQ.iterator();
-                            while (iter.hasNext()) {
-                                minQ.offer((Integer)(iter.next()));
-                            }
-                        }
-                        maxQ.clear();
-                        minQ.remove();
+                        maxQ.remove(minQ.remove());
                     }
-                    
                     break;
             }
         }
 
-        if (minQ.size() != 0) {
-            answer[1] = minQ.remove();
-            
-            while (minQ.size() >= 2) {
-                minQ.remove();
-            }
-            answer[0] = minQ.remove();
-        }
-        
-        if (maxQ.size() != 0) {
+        if (minQ.size() != 0 || maxQ.size() != 0) {
             answer[0] = maxQ.remove();
-            
-            while (maxQ.size() >= 2) {
-                maxQ.remove();
-            }
-            answer[1] = maxQ.remove();
+            answer[1] = minQ.remove();
         }
         
         return answer;
