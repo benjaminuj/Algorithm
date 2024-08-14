a3877d3ee3e95b74600db37a51e54c5d70731510
YuJin Cho <chosj1526@gmail.com>
2024-08-14 22:01:55 +0900

    [level 3] Title: 블록 이동하기, Time: 11.00 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/3/60063. 블록 이동하기/README.md
+++ 프로그래머스/3/60063. 블록 이동하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 블록 이동하기 - 60063 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60063) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60063?language=java) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 4.29 ms
+메모리: 80.9 MB, 시간: 11.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 18:8:14
+2024년 08월 14일 22:01:46
 
 ### 문제 설명
 
--- 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
@@ -1,83 +1,83 @@
 import java.util.*;
 
 class Solution {
-    int N;
-    int[][] dr = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}; // 하 상 우 좌
+    static int[][] dr = {{-1, 0}, {1, 0}, {0, 1},  {0, -1}}; // 상 하 우 좌
     boolean[][][] visited;
-    int answer;
+    int N;
+    int answer = Integer.MAX_VALUE;
     
     public int solution(int[][] board) {
+        int answer = 0;
         N = board.length;
-        visited = new boolean[2][N][N]; 
+        visited = new boolean[2][N][N];
         
-        bfs(board);
-        return answer;
+        return bfs(board);
     }
-    
-    public void bfs(int[][] board) {
+                       
+    public int bfs(int[][] board) {
         Queue<Info> q = new ArrayDeque<>();
-        q.offer(new Info(0,0, 0,1, 0, 0));
+        q.add(new Info(0, 0, 0, 1, 0, 0));
         visited[0][0][0] = true;
         visited[0][0][1] = true;
         
         while (!q.isEmpty()) {
             Info cur = q.poll();
-            
-            if ((cur.r1 == N-1 && cur.c1 == N-1) || (cur.r2 == N-1 && cur.c2 == N-1)) {
-                answer = cur.cnt;
-                return;
+            if ((cur.x1 == N-1 && cur.y1 == N-1) || (cur.x2 == N-1 && cur.y2 == N-1)) {
+                answer = Math.min(answer, cur.time);
             }
             
-            for (int i =0 ; i < 4; i++) {
-                int nR1 = cur.r1 + dr[i][0];
-                int nC1 = cur.c1 + dr[i][1];
-                int nR2 = cur.r2 + dr[i][0];
-                int nC2 = cur.c2 + dr[i][1];
+            for (int i = 0; i < 4; i ++) {
+                // 수평 이동
+                int nx1 = cur.x1 + dr[i][0];
+                int ny1 = cur.y1 + dr[i][1];
+                int nx2 = cur.x2 + dr[i][0];
+                int ny2 = cur.y2 + dr[i][1];
                 
-                if (isRange(nR1, nC1, nR2, nC2) && board[nR1][nC1] != 1 && board[nR2][nC2] != 1) {
-                    if (!visited[cur.type][nR1][nC1] || !visited[cur.type][nR2][nC2]) {
-                        visited[cur.type][nR1][nC1] = true;
-                        visited[cur.type][nR2][nC2] = true;
-                        q.offer(new Info(nR1, nC1, nR2, nC2, cur.type, cur.cnt+1));
+                if (isValid(nx1, ny1, nx2, ny2, board)) {
+                    if (!visited[cur.dir][nx1][ny1] || !visited[cur.dir][nx2][ny2]) {
+                        q.add(new Info(nx1, ny1, nx2, ny2, cur.dir, cur.time+1));
+                        visited[cur.dir][nx1][ny1] = true;
+                        visited[cur.dir][nx2][ny2] = true;
                     }
                     
-                    int changeType = (cur.type == 0 ? 1 : 0);
-                    // 가로일 때 세로 방향으로 회전
-                    if ((cur.type == 0 && i < 2) || (cur.type == 1 && i > 1)) {
-                        if (!visited[changeType][nR1][nC1] || !visited[changeType][cur.r1][cur.c1]) {
-                            visited[changeType][nR1][nC1] = true;
-                            visited[changeType][cur.r1][cur.c1] = true;
-                            q.offer(new Info(nR1, nC1, cur.r1, cur.c1 , changeType, cur.cnt+1));
+                    // 회전 이동
+                    if ((cur.dir == 0 && (i == 0 || i == 1)) || (cur.dir == 1 && (i == 2 || i == 3))) {
+                        if (!visited[(cur.dir + 1)%2][nx2][ny2] || !visited[(cur.dir + 1)%2][cur.x2][cur.y2]) {
+                            q.add(new Info(nx2, ny2, cur.x2, cur.y2, (cur.dir + 1)%2, cur.time+1));
+                            visited[(cur.dir + 1)%2][nx2][ny2] = true;
+                            visited[(cur.dir + 1)%2][cur.x2][cur.y2] = true;
                         }
                         
-                        if (!visited[changeType][nR2][nC2] || !visited[changeType][cur.r2][cur.c2]) {
-                            visited[changeType][nR2][nC2] = true;
-                            visited[changeType][cur.r2][cur.c2] = true;
-                            q.offer(new Info(nR2, nC2, cur.r2, cur.c2, changeType, cur.cnt+1));
+                        if (!visited[(cur.dir + 1)%2][nx1][ny1] || !visited[(cur.dir + 1)%2][cur.x1][cur.y1]) {
+                            q.add(new Info(nx1, ny1, cur.x1, cur.y1, (cur.dir + 1)%2, cur.time+1));
+                            visited[(cur.dir + 1)%2][nx1][ny1] = true;
+                            visited[(cur.dir + 1)%2][cur.x1][cur.y1] = true;
                         }
                     }
                 }
-            }
+            }       
         }
+        return answer;
     }
     
-    boolean isRange(int r1, int c1, int r2, int c2) {
-        return r1 >= 0 && r1 < N && c1 >= 0 && c1 < N && r2 >= 0 && r2 < N && c2 >= 0 && c2 < N;
+    public boolean isValid(int x1, int y1, int x2, int y2, int[][] board) {
+        return x1 >= 0 && y1 >= 0 && x2 >= 0 && y2 >= 0 && x1 < N && y1 < N && x2 < N && y2 < N
+            && board[x1][y1] == 0 && board[x2][y2] == 0;
     }
-    
+                       
     class Info {
-        int r1, c1;
-        int r2 ,c2;
-        int type; // 방향 (0 = 가로, 1 = 세로)
-        int cnt;
+        int x1, y1;
+        int x2, y2;
+        int dir; // 0 : 가로, 1 : 세로
+        int time;
         
-        Info(int r1, int c1, int r2, int c2, int type, int cnt) {
-            this.r1 = r1;
-            this.c1 = c1;
-            this.r2 = r2;
-            this.c2 = c2;
-            this.type = type;
-            this.cnt = cnt;
+        public Info(int x1, int y1, int x2, int y2, int dir, int time) {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+            this.dir = dir;
+            this.time = time;
         }
     }
 }
