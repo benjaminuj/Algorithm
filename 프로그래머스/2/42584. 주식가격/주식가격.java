9a77d831bdde8c5a9e31b8e66dcd9cf38e736190
YuJin Cho <chosj1526@gmail.com>
2024-08-16 00:31:16 +0900

    [level 2] Title: 주식가격, Time: 28.53 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,22 +4,24 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        Deque<Integer> stack = new ArrayDeque<>();
+        Deque<int[]> stack = new ArrayDeque<>(); //{초, 주식가격}
+        
         for (int i = 0; i < prices.length; i++) {
-            while (!stack.isEmpty() && prices[stack.peekLast()] > prices[i]) {
-                int idx = stack.removeLast();
-                
-                answer[idx] = i - idx;
+            // 가격 떨어짐
+            while (!stack.isEmpty() && stack.peek()[1] > prices[i]) {
+                int[] info = stack.pop();
+                answer[info[0]-1] = i+1 - info[0];
             }
             
-            stack.add(i);
+            stack.push(new int[]{i+1, prices[i]});
         }
         
-        while (!stack.isEmpty()) {
-            int idx = stack.removeLast();
-            answer[idx] = answer.length-1 - idx;
+        while(!stack.isEmpty()) {
+            int second = prices.length;
+            int[] info = stack.pop();
+            answer[info[0]-1] = second - info[0];
         }
-      
+        
         return answer;
     }
 }
