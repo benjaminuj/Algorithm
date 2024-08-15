17952c0b57035b568150e28d13343d0090cda822
YuJin Cho <chosj1526@gmail.com>
2024-08-16 04:46:03 +0900

    [level 1] Title: 신고 결과 받기, Time: 143.37 ms, Memory: 172 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,38 +3,32 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
-        HashMap<String, HashSet<String>> reportMap = new HashMap<>(); // 신고 당한 id , Set(신고 한 id)
-        HashMap<String, Integer> beReportedCnt = new HashMap<>(); // 신고 당한 id, 신고 당한 횟수
-        HashMap<String, Integer> result = new HashMap<>(); // id, 결과 메일 받을 횟수
         
-        for (int i= 0; i < report.length; i++) {
-            String[] info = report[i].split(" ");
-            
-            reportMap.putIfAbsent(info[1], new HashSet<>());
-            if (reportMap.get(info[1]).add(info[0])) {
-                beReportedCnt.putIfAbsent(info[1], 0);
-                beReportedCnt.put(info[1], beReportedCnt.get(info[1])+1);
-            }
-            
-            
+        Map<String, Set<String>> map = new HashMap<>(); // key = 신고 당한 id, value = 신고자들 id
+        Map<String, Integer> mailCnt = new HashMap<>(); // key = 메일 받을 사람 id, value = 받을 메일 개수
+        
+        for (String r : report) {
+            String[] info = r.split(" ");
+            map.putIfAbsent(info[1], new HashSet<>());
+            map.get(info[1]).add(info[0]);
         }
         
-        for (String name : beReportedCnt.keySet()) {
-            if(beReportedCnt.get(name) >= k) {
-                for (String rName : reportMap.get(name)) {
-                    result.putIfAbsent(rName, 0);
-                    result.put(rName, result.get(rName) +1);
+        for (String key : map.keySet()) {
+            if (map.get(key).size() >= k) {
+                List<String> reporters = new ArrayList<>(map.get(key));
+                for (int i = 0; i < reporters.size(); i++) {
+                    mailCnt.putIfAbsent(reporters.get(i), 0);
+                    mailCnt.replace(reporters.get(i), mailCnt.get(reporters.get(i))+1);
                 }
             }
-            
         }
         
-        
-        for (int i =0 ; i < id_list.length; i++) {
-            if (result.containsKey(id_list[i])) {
-                answer[i] = result.get(id_list[i]);
+        for (int i = 0; i < id_list.length; i++) {
+            if (mailCnt.containsKey(id_list[i])) {
+                answer[i] = mailCnt.get(id_list[i]);
             }
         }
+        
         return answer;
     }
 }
