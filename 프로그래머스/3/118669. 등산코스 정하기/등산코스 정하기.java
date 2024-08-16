bcd78307bc3dbae53eaf8bd45063d6303808fd3b
YuJin Cho <chosj1526@gmail.com>
2024-08-16 21:46:24 +0900

    [level 3] Title: 등산코스 정하기, Time: 652.66 ms, Memory: 593 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -1,72 +1,90 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        Arrays.sort(summits);
-        Set<Integer> summitSet = IntStream.of(summits).boxed().collect(Collectors.toSet());
-        Map<Integer, List<Info>> graph = new HashMap<>();
+        int[] answer = new int[2];
+        Arrays.fill(answer, Integer.MAX_VALUE);
         
-        int[] answer = new int[]{0, Integer.MAX_VALUE};
+        Integer[] gatesTemp = Arrays.stream(gates).boxed().toArray(Integer[]::new); // int[] -> Integer[] 방법 암기
+        Integer[] summitsTemp = Arrays.stream(summits).boxed().toArray(Integer[]::new);
+        Set<Integer> gateSet = new HashSet<>(Arrays.asList(gatesTemp));
+        Set<Integer> summitSet = new HashSet<>(Arrays.asList(summitsTemp)); // 배열 -> 리스트: Arrays.asList(배열);
         
-        int[] visited = new int[n+1];
-        Queue<Info> pq = new PriorityQueue<>();
-        Arrays.fill(visited, Integer.MAX_VALUE);
-        
-        for (int i =0 ; i < paths.length; i++) {
-            graph.putIfAbsent(paths[i][0], new ArrayList<>());
-            graph.putIfAbsent(paths[i][1], new ArrayList<>());
+        Map<Integer, List<Integer[]>> graph = new HashMap<>(); // key = 시작 노드, value = {도착노드, 두 노드 사이 소요시간}
+        for (int[] path : paths) {
+            graph.putIfAbsent(path[0], new ArrayList<>());
+            graph.get(path[0]).add(new Integer[]{path[1], path[2]});
             
-            graph.get(paths[i][0]).add(new Info(paths[i][1], paths[i][2]));
-            graph.get(paths[i][1]).add(new Info(paths[i][0], paths[i][2]));
+            graph.putIfAbsent(path[1], new ArrayList<>());
+            graph.get(path[1]).add(new Integer[]{path[0], path[2]});
         }
         
-        for (int i = 0; i < gates.length; i++) {
-            pq.offer(new Info(gates[i], 0));
-            visited[gates[i]] = 0;
-        }
-        
-    
-            
-        while (!pq.isEmpty()) {
-            Info cur = pq.poll();
-
-            if (visited[cur.node] < cur.time || summitSet.contains(cur.node)) {
-                continue;
-            }
-
-            for (Info info : graph.get(cur.node)) {
-                int nextIntensity = Math.max(info.time, cur.time);
-                
-                if (nextIntensity < visited[info.node]) {
-                    visited[info.node] = nextIntensity;
-                    pq.offer(new Info(info.node, nextIntensity));
-                }
-            }
-        }
-
+        Arrays.sort(summits); // !!봉우리 넘버 작은 순서: 정렬!!!!
         for (int summit : summits) {
-            if (visited[summit] < answer[1]) {
-                answer[1] = visited[summit];
-                answer[0] = summit;
+            int[] result = new int[2];
+            result = disktra(n, summit, graph, summitSet, gateSet);
+            
+            // 새로 구한 intensity가 더 작은 경우, 정답값 업데이트
+            if (result[1] < answer[1]) {
+                answer[0] = result[0];
+                answer[1] = result[1];
             }
         }
-        
+    
         return answer;
     }
     
-    class Info implements Comparable<Info> {
-        int node;
-        int time;
+    public int[] disktra(int n, int start, Map<Integer, List<Integer[]>> graph, Set<Integer> summitSet, Set<Integer> gateSet) {
+        Queue<Info> pq = new PriorityQueue<>();
+        pq.offer(new Info(0, start));
         
-        public Info(int node, int time) {
+        int[] answer = new int[2];
+        boolean visited[] = new boolean[n+1];
+        
+        while (!pq.isEmpty()) {
+            Info cur = pq.poll();
+            
+            // 방문한 노드면, 이미 최소 intensity로 업데이트되어있기 때문에 이후 로직x
+            if (visited[cur.node]) continue;
+            
+            // pq에서 뺀 후 방문체크
+            visited[cur.node] = true; 
+            
+            // 현재 노드가 게이트 && 첫 게이트라 answer이 초기값이면: 해당 node랑 intensity를 정답으로
+            if (gateSet.contains(cur.node) && answer[0] == 0) {
+                answer[0] = start;
+                answer[1] = cur.intensity;
+                return answer;
+            }
+            
+            for (Integer[] next: graph.get(cur.node)) {
+                // 봉우리는 하나만 가능: 다음 노드도 봉우리이면 그 루트는 제외
+                if (summitSet.contains(next[0])) continue;
+                
+                if (!visited[next[0]]) pq.offer(new Info(Math.max(cur.intensity, next[1]), next[0]));    
+                
+
+            }
+        } 
+        if (answer[0] == 0) {
+            answer[0] = Integer.MAX_VALUE;
+            answer[1] = Integer.MAX_VALUE;
+        }
+        return answer;
+    }
+    
+    public class Info implements Comparable<Info> {
+        int intensity;
+        int node;
+        
+        public Info(int intensity, int node) {
+            this.intensity = intensity;
             this.node = node;
-            this.time = time;
         }
         
         @Override
-        public int compareTo(Info o) {
-            return this.time - o.time;
+        public int compareTo(Info o) {            
+            return this.intensity - o.intensity;
         }
     }
 }
