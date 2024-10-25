ac2611fea09e95af35d2e148ae30a98ca6aa9b14
YuJin Cho <chosj1526@gmail.com>
2024-10-25 22:27:14 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 632 ms, Memory: 58612 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,51 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+public class Main { 
+	static int[] parent;
+	
+	 public static void main(String args[]) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		 String[] arr = br.readLine().split(" ");
+		 
+		 int V = Integer.parseInt(arr[0]);
+		 int E = Integer.parseInt(arr[1]);
+		 
+		 parent = new int[V];
+		 for (int i =0 ; i < V; i++) {
+			 parent[i] = i;
+		 }
+		 
+		 int[][] graph = new int[E][3];
+		 
+	     for(int i = 0; i < E; i++) {
+	    	 String[] temp = br.readLine().split(" ");
+	    	 
+	    	 int a = Integer.parseInt(temp[0])- 1;
+	    	 int b = Integer.parseInt(temp[1])- 1;
+	    	 int c = Integer.parseInt(temp[2]);
+	    	 
+	    	 graph[i][0] = a;
+	    	 graph[i][1] = b;
+	    	 graph[i][2] = c;
+	     }
+	     
+	     Arrays.sort(graph, (o1, o2) -> o1[2] - o2[2]);
+	     
+	     int cost = 0;
+	     for (int i =0 ; i < E; i++) {
+	    	 int a = graph[i][0];
+	    	 int b = graph[i][1];
+	    	 
+	    	 if (find(a) != find(b)) {
+	    		 cost += graph[i][2];
+	    		 union(a, b);
+	    	 }
+	     }
 
-public class Main {
-static int[] parent;
-static int[][] graph;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
-		parent = new int[V+1];
-		for(int i=1; i<=V;i ++) {
-			parent[i] = i;
-		}
-		graph = new int[E][3];
-		for(int i=0; i<E; i++) {
-			st = new StringTokenizer(br.readLine());
-			graph[i][0] = Integer.parseInt(st.nextToken());
-			graph[i][1] = Integer.parseInt(st.nextToken());
-			graph[i][2] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(graph, (o1,o2)-> o1[2] - o2[2]);
-		System.out.println(kruskal(graph));
-	}
-	public static int find(int x) {
-		if(parent[x] == x) return x;
-		else return find(parent[x]);
-	}
-	public static void union(int x, int y) {
-		x = find(x);
-		y = find(y);
-		if(x == y) return;
-		else parent[y]= x;
-	}
-	public static int kruskal(int[][] graph) {
-		int cost = 0;
-		for(int i=0; i<graph.length; i++) {
-			if(find(graph[i][0]) != find(graph[i][1])) {
-				cost += graph[i][2];
-				union(graph[i][0], graph[i][1]);
-			}
-		}
-		return cost;
-	}
-
+	     System.out.println(cost);
+	 }
+	 
+	 public static int find(int x) {
+		 if (parent[x] == x) {
+			 return x;
+		 }
+		 
+		 int y = find(parent[x]);
+		 parent[x] = y;
+		 return y;
+	 }
+	 
+	 public static void union(int x, int y) {
+		 x = find(x);
+		 y = find(y);
+		 
+		 if (x != y) {
+			 parent[y] = x;
+		 }
+	 }
 }
